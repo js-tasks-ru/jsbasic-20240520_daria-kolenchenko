6ea81169b782c1d3de44ab657bcccda35ec7930b
Darya Kolenchenko <daria.kolenchenko@gmail.com>
2024-05-24 10:21:48 +0200

    add 2-module_1-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+  for (let prop in obj) {
+    if (Number.isFinite(obj[prop])) {
+      result += obj[prop];
+    }
+  }
+  return result;
 }
