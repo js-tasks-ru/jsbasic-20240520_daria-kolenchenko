a67c89dea571f8e76aee4e7b6f858533e9353e5f
Darya Kolenchenko <daria.kolenchenko@gmail.com>
2024-06-02 11:13:26 +0200

    review2 3-module and 4-module

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,9 +1,9 @@
 function highlight(table) {
   for (let i = 0; i < table.rows.length; i++) {
-    if (!table.rows[i].cells[3].hasAttribute('data-available')) {
+    if (!table.rows[i].cells[3].dataset.available) {
       table.rows[i].setAttribute('hidden', true);
     } else {
-      if (table.rows[i].cells[3].getAttribute('data-available') === 'true') {
+      if (table.rows[i].cells[3].dataset.available === 'true') {
         table.rows[i].classList.add('available');
       } else {
         table.rows[i].classList.add('unavailable');
@@ -15,9 +15,8 @@ function highlight(table) {
     if (table.rows[i].cells[2].innerHTML === "f") {
       table.rows[i].classList.add('female');
     }
-    if (parseInt(table.rows[i].cells[1].innerHTML) < 18){
+    if (parseInt(table.rows[i].cells[1].innerHTML) < 18) {
       table.rows[i].style.textDecoration = 'line-through';
     }
   }
-
 }
